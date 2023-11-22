0c5d8f32cc01aa79a296d28d85fdb74b1ff4d619
Jane Kostevich <yana.kostevich18@gmail.com>
2023-11-22 18:59:25 +0200

    axios, API, TransitionGroup, Loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo } from "react";
+import React, { useRef, useState, useMemo, useEffect } from "react";
 // import Counter from "./components/Counter";
 import "./styles/App.css";
 import PostList from "./components/PostList";
@@ -6,33 +6,26 @@ import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import { usePosts } from "./hooks/usePosts";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/loader/Loader";
+import { useFetching } from "./hooks/useFetching";
 
 function App() {
   // const [value, setValue] = useState("text");
-  const [posts, setPosts] = useState([
-    { id: 1, title: "Angular", body: "Aescription" },
-    { id: 2, title: "Vue", body: "Bescription" },
-    { id: 3, title: "React", body: "Cescription" },
-  ]);
-
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
-
   const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
 
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query.toLowerCase())
-    );
-  }, [filter.query, sortedPosts]);
+  useEffect(() => {
+    // отрисовує получені з API дані відразу (наблюдатель mounted)
+    fetchPosts();
+  }, []);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -58,12 +51,20 @@ function App() {
       <hr style={{ margin: "15px 0" }} />
 
       <PostFilter filter={filter} setFilter={setFilter} />
-
-      <PostList
-        remove={removePost}
-        posts={sortedAndSearchedPosts}
-        title="Posts about JS"
-      />
+      {postError && <h1>Error... {postError}</h1>}
+      {isPostsLoading ? (
+        <div
+          style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
+        >
+          <Loader />
+        </div>
+      ) : (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title="Posts about JS"
+        />
+      )}
     </div>
   );
 }
