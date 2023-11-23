dc2849844894c18c1c0c998fa26e99055ae07e8c
Jane Kostevich <yana.kostevich18@gmail.com>
2023-11-23 21:17:32 +0200

    router

---
+++ src/pages/Posts.jsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+// import Counter from "./components/Counter";
+import "../styles/App.css";
+import PostList from "../components/PostList";
+import PostForm from "../components/PostForm";
+import PostFilter from "../components/PostFilter";
+import MyModal from "../components/UI/modal/MyModal";
+import MyButton from "../components/UI/button/MyButton";
+import { usePosts } from "../hooks/usePosts";
+import PostService from "../API/PostService";
+import Loader from "../components/UI/loader/Loader";
+import { useFetching } from "../hooks/useFetching";
+import { getPageCount, getPagesArray } from "../utils/pages";
+import Pagination from "../components/UI/pagination/Pagination";
+
+const Posts = () => {
+  // const [value, setValue] = useState("text");
+  const [posts, setPosts] = useState([]);
+  const [filter, setFilter] = useState({ sort: "", query: "" });
+  const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPage] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers["x-total-count"];
+    setTotalPage(getPageCount(totalCount, limit));
+  });
+
+  console.log(totalPages);
+  useEffect(() => {
+    // отрисовує получені з API дані відразу (наблюдатель mounted)
+    fetchPosts();
+  }, [page]);
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+    setModal(false);
+  };
+
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  const changePage = (page) => {
+    setPage(page);
+  };
+  // const bodyInputRef = useRef();
+
+  return (
+    <div className="App">
+      {/* <Counter /> */}
+      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
+        Create Post
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
+
+      <hr style={{ margin: "15px 0" }} />
+
+      <PostFilter filter={filter} setFilter={setFilter} />
+      {postError && <h1>Error... {postError}</h1>}
+      {isPostsLoading ? (
+        <div
+          style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
+        >
+          <Loader />
+        </div>
+      ) : (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title="Posts about JS"
+        />
+      )}
+      <Pagination page={page} totalPages={totalPages} changePage={changePage} />
+    </div>
+  );
+};
+
+export default Posts;
+
+// через useRef ми можемо напряму получати домтуп до ДОМ елементів, приклад використання див PostForm
